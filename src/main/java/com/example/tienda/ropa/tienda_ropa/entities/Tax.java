312af55fa2f88a781b796dec1261fe3667fd8814
Joaquin Mayor <joaquin88mayor@gmail.com>
2024-10-13 21:36:19 -0300

    fix sice

--- src/main/java/com/example/tienda/ropa/tienda_ropa/entities/Tax.java
+++ src/main/java/com/example/tienda/ropa/tienda_ropa/entities/Tax.java
@@ -51,7 +51,7 @@ public class Tax {
 
     @ManyToMany
     @JoinTable(
-        name = "taxXClotheSold",
+        name = "taxXclotheSold",
         joinColumns= @JoinColumn(name = "tax_id"),
         inverseJoinColumns = @JoinColumn(name = "clothe_id"),
         uniqueConstraints = {@UniqueConstraint(columnNames = {"tax_id","clothe_id"})}
