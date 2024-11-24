c6222e4a1cc55243b601b69d74a24184990a83c0
Joaquin Mayor <joaquin88mayor@gmail.com>
2024-11-24 12:57:35 -0300

    add port bean

--- src/main/java/com/example/tienda/ropa/tienda_ropa/TiendaRopaApplication.java
+++ src/main/java/com/example/tienda/ropa/tienda_ropa/TiendaRopaApplication.java
@@ -2,6 +2,9 @@ package com.example.tienda.ropa.tienda_ropa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TiendaRopaApplication {
@@ -9,5 +12,15 @@ public class TiendaRopaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TiendaRopaApplication.class, args);
 	}
+
+	@Bean
+    public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> {
+            String port = System.getenv("PORT");
+            if (port != null) {
+                factory.setPort(Integer.parseInt(port));
+            }
+        };
+    }
 
 }
